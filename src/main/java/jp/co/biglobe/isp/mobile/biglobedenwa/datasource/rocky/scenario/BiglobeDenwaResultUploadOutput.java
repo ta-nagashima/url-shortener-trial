e63f13d699613c5bd2392e98c544163d2ab9729d
Tomoki Nomura <nomura@localhost.local>
2014-09-07 21:53:00 +0900

    不要ファイル削除

--- src/main/java/jp/co/biglobe/isp/mobile/biglobedenwa/datasource/rocky/scenario/BiglobeDenwaResultUploadOutput.java
+++
@@ -1,9 +0,0 @@
-package jp.co.biglobe.isp.mobile.biglobedenwa.datasource.rocky.scenario;
-
-import jp.co.biglobe.lib.publication.scenario.bobio.BobioHeader;
-import lombok.Getter;
-
-public class BiglobeDenwaResultUploadOutput {
-    @Getter
-    private BobioHeader bobioHeader;
-}
--- src/main/java/jp/co/biglobe/isp/mobile/biglobedenwa/domain/rocky/BiglobeDenwaResultUploadRepository.java
+++
@@ -1,5 +0,0 @@
-package jp.co.biglobe.isp.mobile.biglobedenwa.domain.rocky;
-
-public interface BiglobeDenwaResultUploadRepository {
-    public void upload(RockySuccessFile rockySuccessFile, RockyErrorFile rockyErrorFile);
-}
