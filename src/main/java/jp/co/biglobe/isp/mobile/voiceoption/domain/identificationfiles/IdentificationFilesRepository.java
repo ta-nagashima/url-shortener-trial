c2c107e120ba55ddbfebf988940cacdb4bbb3010
Tomoki Nomura <nomura@localhost.local>
2014-09-09 17:12:53 +0900

    プロダクトコードがテストコードに依存してる部分を削除

--- src/main/java/jp/co/biglobe/isp/mobile/voiceoption/domain/identificationfiles/IdentificationFilesRepository.java
+++
@@ -1,5 +0,0 @@
-package jp.co.biglobe.isp.mobile.voiceoption.domain.identificationfiles;
-
-public interface IdentificationFilesRepository {
-    public void register(IdentificationFiles identificationFiles);
-}
