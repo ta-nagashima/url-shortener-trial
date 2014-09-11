33381fdd933bd9c4c7c730d8e8209e6eb1ed1e57
Tomoki Nomura <nomura@localhost.local>
2014-09-11 16:50:26 +0900

    h2のバージョンアップ

--- etc/script/local/H2Start.sh
+++ etc/script/local/H2Start.sh
@@ -2,4 +2,4 @@
 BIN_DIR=`dirname $0`
 cd $BIN_DIR/../../../etc/library/local
 
-java -cp h2-1.4.177.jar:h2storedprocedure-2.0.jar:../production/charset-2.0.jar org.h2.tools.Server
+java -cp h2-1.4.181.jar:h2storedprocedure-2.0.jar:../production/charset-2.0.jar org.h2.tools.Server
