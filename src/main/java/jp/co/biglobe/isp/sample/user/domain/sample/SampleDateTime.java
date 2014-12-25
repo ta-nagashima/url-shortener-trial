5edf77a3b5aa3e2b86fe745bd614833403715edc
Tomoki Nomura <nomura@localhost.local>
2014-12-25 16:47:28 +0900

    joda-time削除

--- src/main/java/jp/co/biglobe/isp/sample/user/domain/sample/SampleDateTime.java
+++ src/main/java/jp/co/biglobe/isp/sample/user/domain/sample/SampleDateTime.java
@@ -5,12 +5,12 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @ToString(includeFieldNames = false)
 @EqualsAndHashCode
 @AllArgsConstructor
 public class SampleDateTime {
     @Getter
-    private final Date value;
+    private final LocalDateTime value;
 }
